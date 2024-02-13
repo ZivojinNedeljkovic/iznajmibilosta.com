a4e6dbfd01c8cceecf81512694dae0592f0527fe
Zivojin Nedeljkovic <zivojin.nedeljkovic.pa@gmail.com>
2024-02-13 11:11:23 +0100

    added firebase

---
+++ src/lib/firebase/createUserWithEmailAndPassword.ts
@@ -0,0 +1,13 @@
+import { createUserWithEmailAndPassword as _createUserWithEmailAndPassword } from 'firebase/auth'
+import { auth } from './firebase'
+
+async function createUserWithEmailAndPassword(email: string, password: string) {
+  const firebaseResult = await _createUserWithEmailAndPassword(
+    auth,
+    email,
+    password
+  )
+  //   auth.signOut()
+}
+
+export default createUserWithEmailAndPassword
