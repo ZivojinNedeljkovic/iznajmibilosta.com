dd9f8164b4e90e28152183448c55ffbc99f2e616
Zivojin Nedeljkovic <zivojin.nedeljkovic.pa@gmail.com>
2024-02-15 07:33:34 +0100

    removed testing components and routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,5 @@
 import TestClient from '@lib/trpc/test-client'
 
 export default function Home() {
-  return (
-    <main>
-      Home
-      <TestClient />
-    </main>
-  )
+  return <main>Home</main>
 }
