815b5c4140746109b5d706dc337197ae8a41e4d0
Zivojin Nedeljkovic <zivojin.nedeljkovic.pa@gmail.com>
2024-02-16 09:30:22 +0100

    added ui improvements

--- src/features/auth/components/card/card.tsx
+++ src/features/auth/components/card/card.tsx
@@ -1,8 +1,19 @@
 import React, { ComponentProps } from 'react'
 import styles from './card.module.scss'
+import H1 from '@ui/h1'
+import classNames from 'classnames'
+import P from '@ui/p'
 
 function Card(props: ComponentProps<'div'>) {
-  return <div {...props} className={styles.card} />
+  return <div {...props} className={classNames(styles.card, props.className)} />
+}
+
+Card.H1 = function CardH1(props: ComponentProps<typeof H1>) {
+  return <H1 {...props} className={classNames(styles.h1, props.className)} />
+}
+
+Card.P = function CardP(props: ComponentProps<typeof P>) {
+  return <P {...props} className={classNames(styles.p, props.className)} />
 }
 
 export default Card
