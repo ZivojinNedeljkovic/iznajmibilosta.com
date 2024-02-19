254cb6ed5133d1125690c56cb12009380810ab64
Zivojin Nedeljkovic <zivojin.nedeljkovic.pa@gmail.com>
2024-02-19 20:07:01 +0100

    header test commented

--- src/features/layout/components/header/header.tsx
+++ src/features/layout/components/header/header.tsx
@@ -5,20 +5,20 @@ import { cookies } from 'next/headers'
 import initFirebaseAdmin from '@firebase/init-firebase-admin'
 
 async function Header() {
-  const session = cookies().get('session')?.value
+  // const session = cookies().get('session')?.value
 
-  if (!session) return
-  initFirebaseAdmin()
+  // if (!session) return
+  // initFirebaseAdmin()
 
-  const auth = getAuth()
-  const idToken = await auth.verifySessionCookie(session, true)
+  // const auth = getAuth()
+  // const idToken = await auth.verifySessionCookie(session, true)
 
-  const user = await auth.getUser(idToken.uid)
+  // const user = await auth.getUser(idToken.uid)
 
   return (
     <>
       <HeaderMobile />
-      <p>{user.email}</p>
+      {/* <p>{user.email}</p> */}
     </>
   )
 }
