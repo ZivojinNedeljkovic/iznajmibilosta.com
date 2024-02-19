0817d2c8efc95ada66aab34b42e917c8159bc630
Zivojin Nedeljkovic <zivojin.nedeljkovic.pa@gmail.com>
2024-02-19 19:56:38 +0100

    verify email page bug fix

--- src/app/(auth)/potvrda-e-mail-adrese/page.tsx
+++ src/app/(auth)/potvrda-e-mail-adrese/page.tsx
@@ -1,19 +1,16 @@
-'use client'
-import React, { useRef } from 'react'
+import React from 'react'
 import Card from '@auth/components/card'
 import styles from './page.module.scss'
 import TextButton from '@ui/buttons/text-button'
+import SessionEmailLink from '@auth/components/session-email-link'
 
 function VerifyEmailPage() {
-  const emailRef = useRef(sessionStorage.getItem('email'))
-  const email = emailRef.current
   return (
     <main className={styles.wrapper}>
       <Card>
         <Card.H1>Skoro pa gotovo!</Card.H1>
         <Card.P>
-          Poslali smo Vam aktivacioni e-mail na:{' '}
-          <a href={`mailto:${email}`}>{email}</a>
+          Poslali smo Vam aktivacioni e-mail na: <SessionEmailLink />
         </Card.P>
         <Card.P>Ako Van nije stigao e-mail:</Card.P>
         <TextButton type="button">
