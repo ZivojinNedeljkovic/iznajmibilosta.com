6c1ba24df79d7bd589c2f37302992c320f10aad8
Zivojin Nedeljkovic <zivojin.nedeljkovic.pa@gmail.com>
2024-02-20 20:29:28 +0100

    added checkbox input

---
+++ src/features/ui/checkbox/checkbox.tsx
@@ -0,0 +1,38 @@
+import React, { ComponentProps, ReactNode, forwardRef } from 'react'
+import styles from './checkbox.module.scss'
+import CheckedIcon from './checked-icon'
+import UncheckedIcon from './unchecked-icon'
+import classNames from 'classnames'
+
+type Props = {
+  children?: ReactNode
+  className?: string
+  isError?: boolean
+  errorMessage?: string
+  inputProps?: Omit<ComponentProps<'input'>, 'type | className | style'>
+}
+
+const Checkbox = forwardRef<HTMLInputElement, Props>(function Checkbox(
+  { children, className, errorMessage, isError = !!errorMessage, inputProps },
+  ref
+) {
+  return (
+    <div>
+      <label
+        className={classNames(styles.container, className, {
+          [styles.error]: isError,
+        })}
+      >
+        <input ref={ref} {...inputProps} type="checkbox" />
+        <CheckedIcon className={styles['checked-icon']} />
+        <UncheckedIcon className={styles['unchecked-icon']} />
+        <span className={styles.label}>{children}</span>
+      </label>
+      {errorMessage && (
+        <p className={styles['error-message']}>{errorMessage}</p>
+      )}
+    </div>
+  )
+})
+
+export default Checkbox
